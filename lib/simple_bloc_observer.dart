67f0a66f3a3c2f7ba040674950bbc258c0d636ec
Nikola Grujić <grujic.nikola91@gmail.com>
2022-01-31 20:05:48 +0100

    Update simple_bloc_observer.dart

--- lib/simple_bloc_observer.dart
+++ lib/simple_bloc_observer.dart
@@ -1,15 +1,25 @@
-import 'package:bloc/bloc.dart';
+// Copyright 2022 Nikola Grujic. All rights reserved.
+// Use of this source code is governed by a GNU-style license that can be
+// found in the LICENSE file.
 
+import 'package:bloc/bloc.dart';
+import 'package:flutter/foundation.dart';
+
+/// Simple bloc observer
 class SimpleBlocObserver extends BlocObserver {
   @override
   void onTransition(Bloc bloc, Transition transition) {
     super.onTransition(bloc, transition);
-    print(transition);
+    if (kDebugMode) {
+      print(transition);
+    }
   }
 
   @override
   void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
-    print(error);
+    if (kDebugMode) {
+      print(error);
+    }
     super.onError(bloc, error, stackTrace);
   }
 }
