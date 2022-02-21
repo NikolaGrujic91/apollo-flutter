04ab152ef04eb4a9b88002dfd9c96f92ee09b7ec
Nikola Grujić <grujic.nikola91@gmail.com>
2022-02-21 06:08:31 +0100

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,20 +6,17 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 /// Execute test by running command in terminal:
-///  flutter run -t test/widget_test.darts
+///  flutter run -t test/widget_test.dart
 
-import 'package:apollo_flutter/features/countdown_timer/business_logic/timer_bloc.dart';
-import 'package:apollo_flutter/features/countdown_timer/data/ticker.dart';
-import 'package:apollo_flutter/features/days/business_logic/days_bloc.dart';
 import 'package:apollo_flutter/features/days/data/repository/days_repository.dart';
-import 'package:apollo_flutter/features/intervals/business_logic/intervals_bloc.dart';
 import 'package:apollo_flutter/features/intervals/data/repository/intervals_repository.dart';
 import 'package:apollo_flutter/features/plans/business_logic/plans_bloc.dart';
 import 'package:apollo_flutter/features/plans/data/repository/plans_repository.dart';
+import 'package:apollo_flutter/features/plans/presentation/plans_list.dart';
+import 'package:apollo_flutter/util/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:apollo_flutter/main.dart';
 
 void main() {
   late PlansRepository plansRepository;
@@ -33,13 +30,24 @@ void main() {
     intervalsRepository = IntervalsRepository();
   });
 
-  testWidgets('Test plans list swidgets', (WidgetTester tester) async {
+  testWidgets('Test plans list widgets', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(
-      ApolloApp(
-        plansRepository: plansRepository,
-        daysRepository: daysRepository,
-        intervalsRepository: intervalsRepository,
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Apollo',
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Apollo'),
+            backgroundColor: kBackgroundColor,
+            foregroundColor: kTextColor,
+          ),
+          body: BlocProvider(
+            create: (_) =>
+                PlansBloc(repository: plansRepository)..add(PlansFetched()),
+            child: const PlansList(),
+          ),
+        ),
       ),
     );
 
